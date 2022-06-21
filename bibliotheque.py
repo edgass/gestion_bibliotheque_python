c45d93ec0d9c35cd7a8d55352f0940edc264dba9
edgass <edaligassama@gmail.com>
2022-06-21 22:51:13 +0000

    declare variables

--- bibliotheque.py
+++ bibliotheque.py
@@ -1,12 +1,15 @@
 
 from cgitb import text
-from tkinter import ttk
+from tkinter import Button, StringVar, ttk
 from re import M
 from textwrap import fill
 from tkinter import BOTH, Entry, Frame, Label, Scrollbar, ttk
+import tkinter
 from turtle import left, right
+import tkinter.messagebox
 
 from mysqlx import Column
+from setuptools import Command
 
 
 class Bibliotheque:
@@ -35,7 +38,21 @@ class Bibliotheque:
         RightFrame1a = Frame(RightFrame1,bd=5,width=90,height=300,padx=2,pady=2)
         RightFrame1a.pack(side="top")
 
-        #===================================================================================================
+        #==========================Declaration de variables================================================
+        code = StringVar()
+        name = StringVar()
+        author = StringVar()
+        description = StringVar()
+        quantity = StringVar()
+
+        #==================================Functions=================================================================
+
+        def iExit():
+            iExit = tkinter.messagebox.askyesno("Attention","Voulez-vous vraiment quitter ?")
+            if iExit > 0 : 
+                root.destroy()
+                return
+        #=============================================================        
 
   
         self.labTitle = Label(TitleFrame,font=('arial',30,'bold'),text="Gestion Bibliotheque")
@@ -86,6 +103,25 @@ class Bibliotheque:
 
         self.list_livres.pack(fill=BOTH,expand=1)
 
+
+        #====================================Boutons CRUD===============================
+
+
+        self.btnCreate = Button(RightFrame1a,font=('arial',16,'bold'),text="Ajouter",bd=4,pady=1,padx=24,
+            width=8,height=2,bg='#4bd650').grid(row=0,column=0,padx=1)
+        self.btnRead = Button(RightFrame1a,font=('arial',16,'bold'),text="Afficher",bd=4,pady=1,padx=24,
+            width=8,height=2,bg='#4bd650').grid(row=1,column=0,padx=1)
+        self.btnUpdate = Button(RightFrame1a,font=('arial',16,'bold'),text="Mettre à jour",bd=4,pady=1,padx=24,
+            width=8,height=2,bg='#d6ba4b').grid(row=2,column=0,padx=1)
+        self.btnDelete = Button(RightFrame1a,font=('arial',16,'bold'),text="Supprimer",bd=4,pady=1,padx=24,
+            width=8,height=2,bg='#c43710').grid(row=3,column=0,padx=1)
+        self.btnSearch = Button(RightFrame1a,font=('arial',16,'bold'),text="Rechercher",bd=4,pady=1,padx=24,
+            width=8,height=2,bg='#106ac4').grid(row=4,column=0,padx=1)
+        self.btnReset = Button(RightFrame1a,font=('arial',16,'bold'),text="Réinitialiser",bd=4,pady=1,padx=24,
+            width=8,height=2,bg='#e6eaeb').grid(row=5,column=0,padx=1)
+        self.btnExit = Button(RightFrame1a,font=('arial',16,'bold'),text="Quitter",bd=4,pady=1,padx=24,
+            width=8,height=2,bg='#ff3700',command=iExit).grid(row=6,column=0,padx=1)
+       
 
        
 
